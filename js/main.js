fc2047bd6461989af7d2d2d1d5a385ea3f7d8007
Regina Galvez Chavez <reginagalvezchavez@MBP-de-Regina.iptv.microsoft.com>
2017-01-30 20:58:04 -0300

    subiendo cambios

--- js/main.js
+++ js/main.js
@@ -1,102 +1,51 @@
-
-function validateForm(){
-
-    var inputname = document.getElementById('name').value;
-    var containerName = document.querySelector('.name-container');
-    var errorName= document.createElement('span');
-    if( inputname == null || inputname.length == 0|| /^\s+$/.test(inputname) ) {
-        errorAlerta = document.createTextNode('Parece que no has ingresado tu nombre');
-        errorName.appendChild(errorAlerta);
-        containerName.appendChild(errorName);
-        return false;
-    }else if (inputname.charAt(0).toUpperCase() !== inputname.charAt(0)){
-        errorAlerta = document.createTextNode('La Primera Letra Debe Ser MaYusCuLa');
-        errorName.appendChild(errorAlerta);
-        containerName.appendChild(errorName); 
-        return false;
-    }else if (inputname.match(/[^a-zA-Z]+/g)){
-        errorAlerta = document.createTextNode('Por favor solo ingresa letras');
-        errorName.appendChild(errorAlerta);
-        containerName.appendChild(errorName); 
-        return false;
-    }
-    
-    var inputLastName = document.getElementById('lastname').value;
-    var containerLastName = document.querySelector('.lastname-container');
-    var errorLastName = document.createElement('span');
-    if( inputLastName == null || inputLastName.length == 0|| /^\s+$/.test(inputLastName) ) {
-        errorAlerta = document.createTextNode('Parece que no has ingresado tu apellido');
-        errorLastName.appendChild(errorAlerta);
-        containerLastName.appendChild(errorLastName);
-        return false;
-    }else if(inputLastName.charAt(0).toUpperCase() !== inputLastName.charAt(0)){
-        errorAlerta = document.createTextNode('La Primera Letra Debe Ser MaYusCuLa');
-        errorLastName.appendChild(errorAlerta);
-        containerLastName.appendChild(errorLastName);
-        return false;
-    }else if (inputLastName.match(/[^a-zA-Z]+/g)){
-        errorAlerta = document.createTextNode('Por favor solo ingresa letras');
-        errorLastName.appendChild(errorAlerta);
-        nameContainer.appendChild(errorLastName); 
-        return false;
-    }
-
-    var mail = document.getElementById('input-email').value;
-    var mailContenedor = document.querySelector('.email-container');
-    var malmail = document.createElement('span');
-    var cosas = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if(mail.length == 0){
-        warning = document.createTextNode('ingresa tu email');
-        malmail.appendChild(warning);
-        mailContenedor.appendChild(malmail);
-        return false;
-    }else if ( !cosas.test(mail)){
-        warning = document.createTextNode('ingresa caracteres validos');
-        malmail.appendChild(warning);
-        mailContenedor.appendChild(malmail);   
-    }
-
-    var pass = document.getElementById('input-password').value;
-    var contenedorPass =  document.getElementById('input-password').parentNode;
-    var errorPass = document.createElement('span');
-    if( pass == 0 ){
-        passMensaje = document.createTextNode('Hola necesitamos tu contraseña');
-        errorPass.appendChild(passMensaje);
-        contenedorPass.appendChild(errorPass);
-        return false;
-    }else if( pass === "123456" || pass === "098754"){
-        passMensaje = document.createTextNode('Hola no puedes usar 123456 ni 098754');
-        errorPass.appendChild(passMensaje);
-        contenedorPass.appendChild(errorPass);
-        return false;
-    } else if(pass.length < 6){
-        passMensaje = document.createTextNode('Revisa que tu contraseña sea mayor de 6 digitos');
-        errorPass.appendChild(passMensaje);
-        contenedorPass.appendChild(errorPass);            
-    }
-    
-    var qbici = document.getElementsByTagName('select')[0].value;
-    var biciContainer = document.getElementsByTagName('select')[0].parentNode;
-    var errorBici = document.createElement('span');
-    var moreInfo= document.querySelector(".checkbox span");
-    if( qbici == null || qbici == 0 ) {
-        warningBici = document.createTextNode('Por favor selecciona tu bicicleta');
-        errorBici.appendChild(warningBici);
-        biciContainer.appendChild(errorBici);
-        return false;
-    }  if( !moreInfo.checked ) {
-        return false;
-    }
-    return true;
-}
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function(){
+  $('button').click(function(){
+  // Borrar los span 
+    $('.borrar').remove();
+// variables para verificar
+    var nombre = $('#name').val();
+    var apellido = $('#lastname').val();
+    var email = $('#input-email').val();
+    var passwrd = $('#input-password').val();
+    var qBici = $('select').val();
+    (function validoNombre(){ 
+        if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)){
+          $('.name-container').append('<span class="borrar" >Hola este campo es obligatorio</span>');
+        } else if (/^[a-zA-Z]*$/.test(nombre) == false){
+          $('.name-container').append('<span class="borrar" >Por favor ingresa sólo letras</span>');
+        } else if (nombre.charAt(0).toUpperCase() !== nombre.charAt(0)){
+           $('.name-container').append('<span class="borrar" >La inicial debe ser mayúscula</span>');
+        }
+     })();
+    (function validoApellido(){ 
+        if (apellido == null || apellido.length == 0 || /^\s+$/.test(apellido)){
+          $('.lastname-container').append('<span class="borrar" >Hola este campo es obligatorio</span>');
+          } else if (/^[a-zA-Z]*$/.test(apellido) == false){
+          $('.lastname-container').append('<span class="borrar" >Por favor ingresa sólo letras</span>');
+          } else if (apellido.charAt(0).toUpperCase() !== apellido.charAt(0)){
+           $('.lastname-container').append('<span class="borrar" >La inicial debe ser mayúscula</span>');
+          }
+    })  ();
+    (function validoCorreo(){
+        if (email == null || email.length == 0){
+          $('.email-container').append('<span class="borrar" >Hola este campo es obligatorio</span>');
+        } else if (/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email) != true){
+          $('.email-container').append('<span class="borrar" >Hola este campo es obligatorio</span>');
+        }
+    }) ();
+    (function validoPass(){
+        if (passwrd.length == 0 ){
+          $('.form-group:first').append('<span class="borrar" >Hola este campo es obligatorio</span>');      
+        } else if (passwrd.length < 6){
+          $('.form-group:first').append('<span class="borrar" >Hola elige una contraseña mayor a 6 caracteres</span>');  
+        } else if(passwrd == 123456 || passwrd == 098754){
+          $('.form-group:first').append('<span class="borrar" >Hola elige una contraseña mas segura</span>');
+        }
+     }) ();
+    (function selectBici(){
+        if( qbici == null || qbici == 0 ){
+          $('.form-group:eq(1)').append('<span class="borrar" >Ya estas por terminar,por favor escoge una bici</span>');  
+        }
+    }) ();
+  });
+});
